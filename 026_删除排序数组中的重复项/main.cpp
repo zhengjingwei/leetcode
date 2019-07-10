8a864f019a897b7b1a3321fb6c615f009c36661d
jwzheng <511894306@qq.com>
2019-07-10 17:02:34 +0800

    026 删除排序数组中的重复项（数组，双指针）

--- 026_删除排序数组中的重复项/main.cpp
+++ 026_删除排序数组中的重复项/main.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
-        if (!nums.size()) return 0;
-        int ret = nums.size();
+        if (nums.size()<=1) return nums.size();
         int i = 0; // 慢指针
-
         for(int j = 1; j < nums.size() ; j ++){ // 快指针
-            while(nums[j] != nums[i]) {
+            if(nums[j] != nums[i]) {
                 i++;
                 nums[i] = nums[j]; // 复制值
             }
