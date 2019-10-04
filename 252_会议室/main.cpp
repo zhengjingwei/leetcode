78a3fe6a278f5aee26d7b009fa696d0beb5269dc
jwzheng <511894306@qq.com>
2019-10-04 11:57:02 +0800

    252 会议室（数组）

---
+++ 252_会议室/main.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if(intervals.empty())
+            return true;
+        sort(intervals.begin(), intervals.end());
+        for(int i = 0; i < intervals.size()-1;++i){
+            if(intervals[i][1] > intervals[i+1][0])
+                return false;
+        }
+        return true;
+    }
+};
